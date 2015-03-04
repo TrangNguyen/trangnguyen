fcef4875f4f3861d68e90ea668376af588a5242f
Trang <trangnguyen80@gmail.com>
2015-03-04 15:42:07 +0100

    using ng-repeat
    compile the abstract to bind link to API instance.

--- app/modules/main.js
+++ app/modules/main.js
@@ -30,9 +30,11 @@ angular.module("trangApp").controller('AppController', ['$scope', '$location', '
 
   //check location.hash and scroll to section
   if($location.hash()) {
-    var scrollId = $location.hash();
-    var section = angular.element(document.getElementById(scrollId));
-    $document.scrollToElementAnimated(section);
+    $timeout(function() {
+      var scrollId = $location.hash();
+      var section = angular.element(document.getElementById(scrollId));
+      $document.scrollToElementAnimated(section);
+    }, 300);
   }
  
   // to chang words on me section.
@@ -53,30 +55,34 @@ angular.module("trangApp").controller('AppController', ['$scope', '$location', '
       }, 1000);
     } else {
       i = 0;
-      $timeout(function() {
-        changeWord(i);
-      }, 1000);
+      changeWord(i);
     }    
   }
   changeWord(0);
   
   /* Videogular stuff */
   
-  /* custom function to seek and play using API */
-  $scope.seekAndPlay = function(second, id) {
-    var video = angular.element(document.getElementById(id));
-    $document.scrollTo(video);
-    //TODO: find the scope.
-    var thing = angular.element(document.getElementsByName('vg-controls')).scope();
-    console.log(thing);
-//    $scope.API.seekTime(second, false);    
-//    $scope.API.play();
+  
+  // player configuration for videgular
+  $scope.config = {
+  	playsInline: false,
+  	autoHide: false,
+  	autoHideTime: 3000,
+  	autoPlay: false,
+  	responsive: true,
+  	loop: false,
+  	preload: "auto",
+  	theme: {
+  		url: "css/app.css"
+  	}
   };
   
-	//media to be passed onto custom directive wrapping videogular
-	$scope.filmclips = [
+	/* media to be passed onto custom directive wrapping videogular */
+	$scope.projects = [
 		{
-		  abtract: '<p> A web app for collaborative sketching. Built on Nodejs, MongoDb,<button ng-click="seekAndPlay(3, \'veryInk-video\')">Angularjs,</button> Raphael.js and <button ng-click="seekAndPlay(7, \'veryInk-video\')">Requirejs.</button></p>',
+		  id: "very-ink",
+		  title: "Very.ink",
+		  abstract: '<p> A web app for collaborative sketching. Built on Nodejs, MongoDb,<button ng-click="seekAndPlay(3, \'video-very-ink\')">Angularjs,</button> Raphael.js and <button ng-click="seekAndPlay(7, \'video-very-ink\')">Requirejs.</button></p>',
       sources: [
 //        {src: $sce.trustAsResourceUrl("media/big_buck_bunny.mp4"), type: "video/mp4"},
 //        {src: $sce.trustAsResourceUrl("media/big_buck_bunny.webm"), type: "video/webm"},
@@ -85,38 +91,26 @@ angular.module("trangApp").controller('AppController', ['$scope', '$location', '
       ]
     },    
     {
-		  abstract: '<p>showcasing the French-German firm\'s works in searchable categories. A complete custom CMS built in Angularjs pulling <button ng-click="seekAndPlay(2, \'lml-video\')">WordPress JSON API</button> topped with Isotope for masonry effects and with Pascal Precht\'s translation module for <button ng-click="seekAndPlay(3, \'lml-video\')">bilingual features.</button> </p>',
+		  id: "lml",
+		  title: "Digital Portfolio for Levin-Monsigny Landscape Architects",
+		  abstract: '<p>showcasing the French-German firm\'s works in searchable categories. A complete custom CMS built in Angularjs pulling <button ng-click="seekAndPlay(2, \'video-lml\')">WordPress JSON API</button> topped with Isotope for masonry effects and with Pascal Precht\'s translation module for <button ng-click="seekAndPlay(3, \'video-lml\')">bilingual features.</button> </p>',
       sources: [
 //        {src: $sce.trustAsResourceUrl("media/big_buck_bunny.mp4"), type: "video/mp4"},
 //        {src: $sce.trustAsResourceUrl("media/big_buck_bunny.webm"), type: "video/webm"},
-        {src: $sce.trustAsResourceUrl("media/big_buck_bunny.ogv"), type: "video/ogg"}
-//          {src: $sce.trustAsResourceUrl("http://v2v.cc/~j/theora_testsuite/stockholm-vfr.ogg"), type: "video/ogg"}
+//        {src: $sce.trustAsResourceUrl("media/big_buck_bunny.ogv"), type: "video/ogg"}
+          {src: $sce.trustAsResourceUrl("http://v2v.cc/~j/theora_testsuite/stockholm-vfr.ogg"), type: "video/ogg"}
       ]
     }
   ];
   
-  // player configuration for videgular
-  $scope.config = {
-		playsInline: false,
-		autoHide: false,
-		autoHideTime: 3000,
-		autoPlay: false,
-		responsive: true,
-		loop: false,
-		preload: "auto",
-		theme: {
-			url: "css/app.css"
-		}
-	};
 }]);
 
-angular.module("trangApp").directive('trangVideogular', function(){
+angular.module("trangApp").directive('trangVideogular', function($document){
   return {
     restrict: 'E',
     scope: {
       filmclip:'=',//vg-src and abstract
-      config:'=',//player config,
-      abstract:'='
+      config:'='//player config,
     },
     templateUrl: 'modules/templates/trang-videogular-directive.html',
     link: function(scope) {
@@ -125,6 +119,33 @@ angular.module("trangApp").directive('trangVideogular', function(){
       scope.onPlayerReady = function(API) {
         scope.API = API;
       };
+      //bind custom controller to specific API
+      scope.seekAndPlay = function(second, id) {
+        var video = angular.element(document.getElementById(id));
+        $document.scrollTo(video);
+        scope.API.seekTime(second, false);    
+        scope.API.play();
+      };
     }
+  };
+});
+
+// directive to compile html element as is.
+angular.module("trangApp").directive('compile', function($compile) {
+  // directive factory creates a link function
+  return function(scope, element, attrs) {
+    scope.$watch(
+      function(scope) {
+         // watch the 'compile' expression for changes
+        return scope.$eval(attrs.compile);
+      },
+      function(value) {
+        // when the 'compile' expression changes assign it into the current DOM
+        element.html(value);
+  
+        // compile the new DOM and link it to the current scope. We only compile .childNodes so that we don't get into infinite loop compiling ourselves
+        $compile(element.contents())(scope);
+      }
+    );
   };
 });
