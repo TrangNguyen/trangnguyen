b87509adaf1986b04d0b4d0b6ac2d980a9546e67
Trang <trangnguyen80@gmail.com>
2015-04-20 20:45:44 +0200

    Made a bunch of style changes

--- app/modules/main.js
+++ app/modules/main.js
@@ -76,24 +76,24 @@ angular.module("trangApp").controller('AppController', ['$scope', '$location', '
   //about me
   $scope.reasons = [{
     head: 'I’m a self-taught developer',
-    description: 'I’m an autodidact developer and a cross-disciplinarian. Having graduated in film editing, I make <a target="_blank" href="http://midcut.com">films</a>, especially photofilms and installations. While looking for a technological solution to my video installation work, I was introduced to NodeJS and I was thrilled. Later with MongoDB and AngularJS I made myself a full-stack developer.'
+    description: 'I studied Computer Science Education, Ecology and most recently Film, (see my <a target="_blank" href="http://midcut.com">film</a> portfolio). My video installation work lead me to development and I use what I learned in Film in my development work.'
   },{
     head: 'I’m a doer',
-    description: 'Whatever I set myself into, I’ve excelled at it, whether it’s making exquisite meals or knitting a motherboard pattern. I take great pride in translating challenging design concepts into shipping applications and find an incredible reward seeing the result of my hours of hard work online.'
+    description: 'I take great pride in translating challenging design concepts into working applications and love seeing the result of many hours of work online.'
   },{
     head: 'I’m a team player',
-    description: 'As a filmmaker, I’ve mastered teamwork. There isn’t a product that is just for me to keep on my shelf but to share, to communicate. I respect my client’s interest, appreciate a good design, put myself in the shoes of the ones who will be using the product and combine all input, critic for the best of the project.'
+    description: 'I respect my client’s interest and I love working on a team to work together to build a great product.'
   },{
     head: 'I’m a perfectionist',
-    description: 'I love planning, I do extensive research, prototype, test and iterate, that goes for coding as much as for filmmaking. A project is not finished until I have seen the result, had a wrap-up with involved parties and drawn lessons for the next.'
+    description: 'I do extensive research, prototyping, testing and iteration. A project is finished once I have seen the result, have had a wrap-up with involved parties and drawn lessons for the next job.'
   }];
   
   //recent projects  
   $scope.projects = [{
     title: 'Levin-Monsigny Landschaftsarchitekten',
     id:'levin-monsigny',
-    intro: 'With their successful international expansion, the French-German landscape architechture firm needed a new internet presence to showcase their works in various themes and countries.',
-    process: 'The concept and design was made by minigram. I teamed up with Berliner Süden to develop from scratch an Angular application with backend in WordPress serving as API. The result is an application that allows visitor to easily browse Levin-Monsigny Landschaftsarchitekten’s works in different languages, filters and sorting possibilities. Each project has flexible layout, a touch-friendly slideshow, suggestion of similar projects as well a shortcut to other project categories.',
+    intro: 'With their successful international expansion, the French-German landscape architecture firm needed a new internet presence to showcase their works across the globe.',
+    process: 'The concept and design was made by minigram. I was hired by Berliner Süden to develop an Angular application with a WordPress backend serving as a content API. The result is an application that allows visitors to explore Levin-Monsigny Landschaftsarchitekten’s projects using different languages, filters and sorting possibilities. Each project entry makes use of flexible layout modules, a touch-friendly slideshow, suggestions of similar projects as well a shortcut to other project categories.',
     team: [
       { name: "minigram Studio für Markendesign",
         role: "Design",
@@ -115,7 +115,7 @@ angular.module("trangApp").controller('AppController', ['$scope', '$location', '
     title: 'City & Home',
     id: 'city-and-home',
     intro: 'After 12 years on the market, the real estate developer City and Home approached Berliner Süden for a new website showcasing their high quality projects, from residential houses and apartments to commercial estates and office buildings.',
-    process:'Berliner Süden designed the app and I developed a mobile-friendly Angular application pulling WordPress JSON API with an extensive estate search feature, scrolling navigation for quick content access, intuitive touch slideshow and asynchronous integration of Google Maps API, all lazy loaded with RequireJS.',
+    process:'Berliner Süden designed the app and I developed a mobile-friendly Angular application pulling WordPress JSON API with an extensive estate search feature, scrolling navigation for quick content access, an intuitive touch-friendly slideshow and asynchronous integration of Google Maps API.',
     team: [ 
       { name: "Berliner Süden",
         role: " Design, WordPress & CSS Development",
@@ -132,16 +132,16 @@ angular.module("trangApp").controller('AppController', ['$scope', '$location', '
     title: 'Random Embassy',
     id: 'random-embassy',
     intro: 'As Smyrski Creatives turned Random Embassy, the Philadelphia-based creative studio approached Berliner Süden for a new digital portfolio.',
-    process: 'To showcase the Philadelphia-based creative studio Random Embassy’s various projects and design activities, I developed an Angular application that is optimized for mobile, with dynamic content loading, unique layout for every featured project and smooth transition combining AngularJS and CSS Animation. While Berliner Süden took care of WordPress backend data design and CSS-Development and Anthony Smyrski tuned the design, I optimized WordPress JSON API to feed the Angular app and  developed the app’s features.',
+    process: 'To showcase the Philadelphia-based creative studio Random Embassy’s various projects and design activities, I developed an Angular application that is optimized for mobile, with dynamic content loading, unique layout for every featured project and smooth transition combining AngularJS and CSS Animation. While Berliner Süden took care of WordPress backend data design and CSS-Development, I optimized the WordPress JSON API to feed content to the Angular app.',
     team: [ 
       { name: "Anthony Smyrski",
         role: "Design",
         link: "http://randomembassy.com/#/"
       }, { name: "Berliner Süden",
-        role: "WordPress & CSS Development </>",
+        role: "WordPress & CSS Development",
         link: "http://www.berlinersueden.de"
       }, { name: "Trang Nguyen",
-        role: "WordPress & Angular Development {{ }}"
+        role: "WordPress & Angular Development"
       } 
     ],
     image: 'media/randomembassy/random1.jpg',
