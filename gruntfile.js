9dd161a6b6162bb533c18f0f60b8ab8d5f55e882
Trang <trangnguyen80@gmail.com>
2015-04-20 21:15:17 +0200

    fixed gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -45,7 +45,7 @@ module.exports = function (grunt) {
     uglify: {
       dist: {
         files: {
-          '<%= outputDir %>/main.js': [  '<%= bower.directory %>/angular/angular.js',
+          '<%= outputDir %>/main.js': [  '<%= bower.directory %>/angular/angular.js', '<%= bower.directory %>/angular-scroll/angular-scroll.min.js',
           '<%= outputDir %>/main.js'
          ]
         },
